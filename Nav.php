bf3a1fb2ad8c8f4a6e9e8c145255d80e043ebf1d
hideki_okajima <hideki_okajima@lockon.co.jp>
2018-11-29 15:59:33 +0900

    generate plugin on command

---
+++ Nav.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Plugin\TagSample;
+
+use Eccube\Common\EccubeNav;
+
+class Nav implements EccubeNav
+{
+    /**
+     * @return array
+     */
+    public static function getNav()
+    {
+        return [];
+    }
+}
